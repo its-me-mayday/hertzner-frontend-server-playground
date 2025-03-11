289342933a094097bb60fc8d4e11ff4838ed2ea9
Luca Maggio <lucamaggio1992@gmail.com>
2025-03-11 12:02:50 +0100

    chore(server): apply variables

--- server.tf
+++ server.tf
@@ -1,8 +1,8 @@
 resource "hcloud_server" "frontend_server" {
   name        = var.hcloud_server_name
-  image       = "ubuntu-22.04"
-  server_type = "cax11"
-  location    = "fsn1"
+  image       = var.hcloud_server_image_name
+  server_type = var.hcloud_server_type_name
+  location    = var.hcloud_server_location_name
   ssh_keys    = [hcloud_ssh_key.ssh_public_key.name]
   public_net {
     ipv4_enabled = true
