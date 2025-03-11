a8bb4f3be16c8b9209b7c018ecde2643d53f5933
Luca Maggio <luca.maggio@activadigital.it>
2025-03-11 11:09:33 +0100

    chore: respond only a single dns

--- dns.tf
+++ dns.tf
@@ -6,3 +6,12 @@ resource "cloudflare_record" "frontend" {
   proxied = false
   content = "88.99.32.249"
 }
+
+resource "cloudflare_record" "isnot-frontend" {
+  zone_id = var.cloudflare_zone_id
+  name    = "isnot-frontend.lucamaggio.xyz"
+  type    = "A"
+  ttl     = 3600
+  proxied = false
+  content = "88.99.32.249"
+}
